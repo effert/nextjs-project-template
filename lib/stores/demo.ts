2648eaaf4450d7153b0b079511b578445c9c9743
effert <yanhan@shinemo.com>
2023-07-14 17:19:53 +0800

    fix(server): fix TypeScript error in server code

--- lib/stores/demo.ts
+++ lib/stores/demo.ts
@@ -1,6 +1,11 @@
 import { create } from 'zustand';
 
-const useStore = create((set, get, api) => ({
+interface BearState {
+  bears: number;
+  increasePopulation: () => void;
+  removeAllBears: () => void;
+}
+const useStore = create<BearState>((set, get, api) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
